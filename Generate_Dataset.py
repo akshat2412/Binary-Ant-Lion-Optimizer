ab6aeecbdd5882cec6ade36a1ba8c3bd05f109ed
Gitesh-Narula <giteshnarulamait@gmail.com>
2018-04-10 07:52:57 +0530

    Updated Generate Datasets

--- Generate_Dataset.py
+++ Generate_Dataset.py
@@ -1,4 +1,8 @@
-def GenerateData():	
-	from sklearn.datasets import make_classification
-	X, y = make_classification(n_samples=100000, n_features=60,n_informative=20,n_redundant=10,random_state=42)
-	return X,y
+import csv
+import pandas as pd
+def GenerateData1():
+	data=pd.read_csv('Heart_data.csv')
+	X=data.iloc[:,:44]
+	y=data.iloc[:,44]
+	return X,y	
+	
